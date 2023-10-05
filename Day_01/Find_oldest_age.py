35bccddc50929d4e609ffb7b6d45757564a2892f
adilhayat106 <adilhayatramay@gmail.com>
2023-10-06 02:05:57 +0500

    add new folder

---
+++ Day_01/Find_oldest_age.py
@@ -0,0 +1,9 @@
+# input
+age_1 = int(input('enter 1st birth year'))
+age_2 = int(input('enter 2nd birth year'))
+age_3 = int(input('enter 3rd birth year'))
+
+# Compare all ages to find the oldest age using min function
+old_age = min(age_1, age_2, age_3)
+
+print("Oldest age is:", old_age)
